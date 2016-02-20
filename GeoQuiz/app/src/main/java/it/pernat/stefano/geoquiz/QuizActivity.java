a539e022c99f102803e65d40ddae9ccf0057011e
Stefano Pernat <stefano.pernat@gmail.com>
2016-02-20 16:52:59 +0100

    Added button logic

--- GeoQuiz/app/src/main/java/it/pernat/stefano/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/it/pernat/stefano/geoquiz/QuizActivity.java
@@ -8,15 +8,46 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO implement on click for true button
+                Toast.makeText(
+                        QuizActivity.this,
+                        getString(R.string.incorrect_toast),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO implement on click listener for false button
+                Toast.makeText(
+                        QuizActivity.this,
+                        getString(R.string.correct_toast),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 
     @Override
