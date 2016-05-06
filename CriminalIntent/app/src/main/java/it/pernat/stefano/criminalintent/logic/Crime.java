0e11544437b9e0ccf4efc4eceb7df9c2890476f1
Stefano Pernat <stefano.pernat@gmail.com>
2016-05-06 21:04:45 +0200

    updated Crime class

--- CriminalIntent/app/src/main/java/it/pernat/stefano/criminalintent/logic/Crime.java
+++ CriminalIntent/app/src/main/java/it/pernat/stefano/criminalintent/logic/Crime.java
@@ -1,5 +1,6 @@
 package it.pernat.stefano.criminalintent.logic;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,9 +9,12 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         this.mId = UUID.randomUUID();
+        this.mDate = new Date();
     }
 
     public UUID getId() {
@@ -21,7 +25,23 @@ public class Crime {
         return this.mTitle;
     }
 
+    public Date getDate() {
+        return this.mDate;
+    }
+
+    public boolean isSolved() {
+        return this.mSolved;
+    }
+
     public void setTitle(String title) {
         this.mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
     }
 }
